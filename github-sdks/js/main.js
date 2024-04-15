71073eabcb3f3c2710d798e783d04509819807ed
Chinedu Olebu <74111891+golebu2020@users.noreply.github.com>
2024-04-15 00:35:04 +0000

    final commit

--- github-sdks/js/main.js
+++ github-sdks/js/main.js
@@ -1,16 +1,18 @@
 // Octokit.js
 // https://github.com/octokit/core.js#readme
-import { Octokit, App} from "octokit";
+const  {Octokit, App} = require("octokit");
 console.log(`TOKEN = ${process.env.GH_TOKEN}`)
 const octokit = new Octokit({
     auth: process.env.GH_TOKEN
   })
   
-  await octokit.request('GET /repos/{owner}/{repo}/git/ref/{ref}', {
+  octokit.request('POST /repos/{owner}/{repo}/git/refs', {
     owner: 'golebu2020',
     repo: 'authrepo',
     ref: 'refs/heads/sdks',
+    sha: '532b49c1c9581af09d1742b478ad295534f75969',
     headers: {
       'X-GitHub-Api-Version': '2022-11-28'
     }
   })
+
